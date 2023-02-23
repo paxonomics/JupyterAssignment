fde5d31e3dea45a528483008a24fb98c877ceb6d
JMS <joacoms@gmail.com>
2023-02-23 13:22:54 +0200

    Add files via upload

---
+++ Jupyter Notebook Assignment.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Joaquín Mercado Sapiaín**
+
+# 25% Data Scientist
+
+# *I´ve always been interested in data-driven decision making, and data science will give me the right tools to use data to inform portfolio investments*
+
+# ### This is a basic mathematical operation *(no decimal points unfortunately)*
+
+# In[11]:
+
+
+3949*44
+
+
+# | Brand | Year | Inventory |
+# | ----- | ----- | -----|
+# | Ion | 2023 | Y |
+# | Duotone | 2023 | N |
+# | North | 2022 | Y |
+# 
+# ~~Duotone out of stock~~
+# 
+# - Alpha
+# - Beta
+# - Delta
+# - Gamma
+# 
+# 1. alpha
+# 2. beta
+# 3. delta
+# 4. gamma
